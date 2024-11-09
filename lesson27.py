2ddd20650cd4a8de5f90570f1b800aeb0f4e5941
Orcil07 <orcil9675@gmail.com>
2024-11-09 23:11:24 +0300

    Add files via upload

---
+++ lesson27.py
@@ -0,0 +1,47 @@
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+    def __init__(self, owner, __model, __engine_power, __color):
+        self.owner = str(owner)
+        self.__model = str (__model)
+        self.__engine_power = int(__engine_power)
+        self.__color = str(__color)
+
+
+    def get_model(self):
+        print(f'Модель: {self.__model}')
+
+    def get_horsepower(self):
+        print(f'Мощность двигателя: {self.__engine_power}')
+
+    def get_color(self):
+        print(f'Цвет: {self.__color.upper()}')
+
+    def print_info(self):
+        self.get_model()
+        self.get_horsepower()
+        self.get_color()
+        print(f'Владелец: {self.owner}\n')
+
+    def set_color(self, new_color):
+        # new_color = input('Новый цвет: ')
+        reg_colours = [colour.lower() for colour in self.__COLOR_VARIANTS]
+        if new_color.lower() in reg_colours:
+            self.__color = new_color
+        else:
+            print(f'Нельзя сменить цвет на {new_color}\n')
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+avto1 = Sedan('Nursultan Tyulyakbaev', 'Plymouth Fury', 155, 'Red')
+
+
+avto1.print_info()
+
+
+avto1.set_color('Горячий снег')
+avto1.set_color('Indigo')
+avto1.owner = 'Azamaz Bogatov'
+
+
+avto1.print_info()
